28c7a2e64c1aa90fd9dab6fc01f226a089760fa5
Jack Sovelove <soveloj@wwu.edu>
2025-02-26 01:04:48 -0800

    .env ignore

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import envCompatible from 'vite-plugin-env-compatible'
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), envCompatible()],
-  base: '/Civil-Rights-History-LLM/', // Change this from '/Civil-Rights-History-LLM/'
+  base: '/civil-rights-history-project/', // Change this from '/Civil-Rights-History-LLM/'
   server: {
     port: 3000,
     open: true, // This will open the browser automatically
