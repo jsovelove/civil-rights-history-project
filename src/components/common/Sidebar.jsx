664d2dd32325723a2ccc15907ab1599556afd671
Jack Sovelove <soveloj@wwu.edu>
2025-09-28 22:41:42 -0700

    Update Sidebar.jsx

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -56,7 +56,7 @@ export default function Sidebar() {
 
       {/* Sidebar */}
       <div
-        className={`fixed top-0 left-0 h-screen w-64 bg-white border-r border-gray-200 shadow-lg transition-transform duration-300 ease-in-out z-50 flex flex-col p-4 font-body ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
+        className={`fixed top-0 left-0 h-screen w-64 bg-white border-r border-gray-200 shadow-lg transition-transform duration-300 ease-in-out z-50 flex flex-col p-4 font-body ${isOpen ? 'translate-x-0' : '-translate-x-full'} overflow-hidden`}
       >
         {/* Close Button */}
         <button
